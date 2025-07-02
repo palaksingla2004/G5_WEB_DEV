1fc9e1ecf26536361699e7204547279420ebe3fb
palaksingla2004 <palaksingla1401@gmail.com>
2025-07-02 10:40:10 +0530

    lec2

---
+++ L2_02_07/script.js
@@ -0,0 +1,53 @@
+// function calculate(param1){
+//     console.log("Calculate the BL");
+//     param1();
+// }
+// var sumoftwo=(){
+//     console.log("SUM OF TWO WORKING");
+// }
+// calculate(sumoftwo);
+
+
+//MAIN HIGHER ORDER FUNCTION
+// function calculate(logicfunction ,a,b){
+//     return logicfunction(a,b);
+// }
+//LOGICAL FUNCIONS
+// function add(a,b){
+//     return a+b;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
+
+// console.log(calculate(add,3,5));
+// console.log(calculate(sub,13,5));
+
+
+
+//Student details
+// function Welcome(logicfunction,value){
+//     return logicfunction(value);
+// }
+// //LOGICAL FUNCIONS
+// function StudentName(StudentName){
+//     return StudentName;    
+// }
+// function fees(Fees){
+//     return Fees;
+// }
+// function FeesEMI(FeesEMI){
+//     emi=FeesEMI/10;
+//     return emi;
+// }
+
+// console.log(Welcome(StudentName,"Palak"));
+// console.log(Welcome(fees,30000));
+// console.log(Welcome(FeesEMI,30000));
+
+
+
+//SETTIMEOUT
+//setTimeout(()=>console.log("Hello"),200)
+//SETINTERVAL
+// setInterval(()=>console.log("hii"),1000)
